a00bad76f5198dee6ece0296fd05b83fc993d1ed
Angel <angel.l.puente@gmail.com>
2023-01-22 19:45:51 -0600

    update pathing again

--- payload.config.js
+++ payload.config.js
@@ -1,8 +1,8 @@
 import { buildConfig } from 'payload/config';
-import Categories from './Backend2/collections/Categories';
-import Posts from './Backend2/collections/Posts';
-import Tags from './Backend2/collections/Tags';
-import Users from './Backend2/collections/Users';
+import Categories from './Backend/collections/Categories';
+import Posts from './Backend/collections/Posts';
+import Tags from './Backend/collections/Tags';
+import Users from './Backend/collections/Users';
 
 export default buildConfig({
   serverURL: 'http://localhost:5000',
